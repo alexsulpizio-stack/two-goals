d358bad4c7bc616537a15f8149683ca913e9be99
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-05 00:21:24 -0400

    Embed AI Guide in Independence

--- src/app/independence/page.tsx
+++ src/app/independence/page.tsx
@@ -1,5 +1,11 @@
+import { GuidePanel } from "@/components/guide-panel";
 import { IndependenceView } from "@/components/independence-view";
 
 export default function IndependencePage() {
-  return <IndependenceView />;
+  return (
+    <div className="flex flex-col gap-10">
+      <GuidePanel />
+      <IndependenceView />
+    </div>
+  );
 }
