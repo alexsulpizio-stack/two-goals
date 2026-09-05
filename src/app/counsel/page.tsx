63d0f53bed4ca7fb4c970988f6aa4ca490f81033
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-05 00:11:48 -0400

    Use Plan Assistant instead of legacy Counsel controller

--- src/app/counsel/page.tsx
+++ src/app/counsel/page.tsx
@@ -1,19 +1,5 @@
-import { CounselAnswerReview } from "@/components/counsel-answer-review";
-import { CounselView } from "@/components/counsel-view";
-import { INTERVIEW_QUESTIONS } from "@/lib/interview";
+import { PlanAssistant } from "@/components/plan-assistant";
 
 export default function CounselPage() {
-  return (
-    <div className="flex flex-col gap-10">
-      <script
-        id="interview-questions"
-        type="application/json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify(INTERVIEW_QUESTIONS).replace(/</g, "\\u003c"),
-        }}
-      />
-      <CounselView />
-      <CounselAnswerReview />
-    </div>
-  );
+  return <PlanAssistant />;
 }
