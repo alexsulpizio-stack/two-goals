2f26493e6c16f3a3a5015c6e0bc4d47e76d5c216
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-04 22:51:28 -0400

    Add Quicken importer to Steward

--- src/app/steward/page.tsx
+++ src/app/steward/page.tsx
@@ -1,3 +1,4 @@
+import { QuickenImport } from "@/components/quicken-import";
 import { StewardInsights } from "@/components/steward-insights";
 import { StewardView } from "@/components/steward-view";
 
@@ -5,6 +6,7 @@ export default function StewardPage() {
   return (
     <div className="flex flex-col gap-10">
       <StewardInsights />
+      <QuickenImport />
       <StewardView />
     </div>
   );
