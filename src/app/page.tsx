bf1cfdc703db06c0249e31eee60efaec7afeb189
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-04 20:40:44 -0400

    Put one next action at the top of Compass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { CompassHome } from "@/components/compass-home";
+import { CompassNext } from "@/components/compass-next";
 
 export default function Home() {
-  return <CompassHome />;
+  return (
+    <div className="flex flex-col gap-10">
+      <CompassNext />
+      <CompassHome />
+    </div>
+  );
 }
